88918c6a488ae691c87c0bdddbb2a2e1f2d2d603
CC-KEH <CC-KEH@github.com>
2022-08-17 07:50:11 -0700

    some more questions

---
+++ src/com/company/AlgorithmsTwo/UniqueMorseCodeWords.java
@@ -0,0 +1,22 @@
+package com.company.AlgorithmsTwo;
+
+public class UniqueMorseCodeWords {
+    public static void main(String[] args) {
+
+    }
+//    class Solution {
+//        public int uniqueMorseRepresentations(String[] words) {
+//            String[] alphabet = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+//            Set<String> set = new HashSet<>();
+//            for (String w: words) {
+//                StringBuilder str = new StringBuilder();
+//                for(char c: w.toCharArray()) {
+//                    c -= 97;
+//                    str.append(alphabet[c]);
+//                }
+//                set.add(str.toString());
+//            }
+//            return set.size();
+//        }
+//    }
+}
